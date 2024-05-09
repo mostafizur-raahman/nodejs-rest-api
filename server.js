acf8cad73e90f5e42c46238bb707527414f3c7cb
mostafizur-raahman <mostafizurrahman@0202@gmail.com>
2024-05-10 00:04:43 +0600

    added default setup in express

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("Starting server");
+const App = require("./src/app.js");
+
+const server = new App();
+
+const serverStart = () => {
+    const PORT = process.env.PORT || 3001;
+
+    server.app.listen(() => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+};
+
+// server start
+serverStart();
