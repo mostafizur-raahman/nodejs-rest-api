5d9d455fdb4418e3e6b6a0b35b2fae0f6191f7df
mostafizur-raahman <mostafizurrahman@0202@gmail.com>
2024-05-10 15:01:42 +0600

    add register user validation

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const globalError = require("./middlewares/globalErrorHandler");
 
 const app = express();
 
+// middleware
+app.use(express.json());
+
 // GET , POST , DELETE
 app.get("/", (req, res, next) => {
     return res.json({
--- src/user/userController.js
+++ src/user/userController.js
@@ -1,7 +1,15 @@
+const createHttpError = require("http-errors");
+
 const createuser = async (req, res, next) => {
-    res.json({
-        message: "Hello World",
-    });
+    const { name, email, password } = req.body;
+
+    // validation
+    if (!name || !email || !password) {
+        const error = createHttpError(400, "Please provide all fields");
+        return next(error);
+    }
+
+    res.json({});
 };
 
 module.exports = {
