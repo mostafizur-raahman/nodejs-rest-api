ca2674aa56d109474b09ac572aabe7c4f9396ea2
mostafizur-raahman <mostafizurrahman@0202@gmail.com>
2024-05-10 15:51:10 +0600

    add error handling

--- src/user/userController.js
+++ src/user/userController.js
@@ -5,44 +5,48 @@ const jwt = require("jsonwebtoken");
 const config = require("../config/config");
 
 const createuser = async (req, res, next) => {
-    const { name, email, password } = req.body;
+    try {
+        const { name, email, password } = req.body;
 
-    // validation
-    if (!name || !email || !password) {
-        const error = createHttpError(400, "Please provide all fields");
-        return next(error);
-    }
-
-    const existingEmail = await User.findOne({ email });
-    if (existingEmail) {
-        const error = createHttpError(400, "Email already exists");
-        return next(error);
-    }
-
-    const hashPassword = await bcrypt.hash(password, 10);
-
-    const newUser = await User.create({
-        name,
-        email,
-        password: hashPassword,
-    });
-
-    // Token
-    const token = jwt.sign(
-        {
-            sub: newUser._id,
-        },
-        config.jwtSecret,
-        {
-            expiresIn: "7h",
+        // validation
+        if (!name || !email || !password) {
+            const error = createHttpError(400, "Please provide all fields");
+            return next(error);
         }
-    );
 
-    return res.status(201).json({
-        message: "User created successfully",
-        newUser: newUser,
-        token: token,
-    });
+        const existingEmail = await User.findOne({ email });
+        if (existingEmail) {
+            const error = createHttpError(400, "Email already exists");
+            return next(error);
+        }
+
+        const hashPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await User.create({
+            name,
+            email,
+            password: hashPassword,
+        });
+
+        // Token
+        const token = jwt.sign(
+            {
+                sub: newUser._id,
+            },
+            config.jwtSecret,
+            {
+                expiresIn: "7h",
+            }
+        );
+
+        return res.status(201).json({
+            message: "User created successfully",
+            newUser: newUser,
+            token: token,
+        });
+    } catch (error) {
+        next(createHttpError(500, "Internal Server Error"));
+    }
 };
 
 module.exports = {
