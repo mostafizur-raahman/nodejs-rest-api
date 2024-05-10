9084ed017411b4ac10862fdc85d28d174119edf7
mostafizur-raahman <mostafizurrahman@0202@gmail.com>
2024-05-10 15:46:38 +0600

    sign jwt token

--- src/config/config.js
+++ src/config/config.js
@@ -4,6 +4,7 @@ const _config = {
     port: process.env.PORT,
     databaseURL: process.env.MONGO_CONNECTION_STRING,
     env: process.env.NODE_ENV,
+    jwtSecret: process.env.JWT_SECRET,
 };
 
 const config = Object.freeze(_config);
--- src/user/userController.js
+++ src/user/userController.js
@@ -1,6 +1,8 @@
 const createHttpError = require("http-errors");
 const User = require("./userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const config = require("../config/config");
 
 const createuser = async (req, res, next) => {
     const { name, email, password } = req.body;
@@ -11,13 +13,36 @@ const createuser = async (req, res, next) => {
         return next(error);
     }
 
-    const existingEmail = User.findOne({ email });
+    const existingEmail = await User.findOne({ email });
     if (existingEmail) {
         const error = createHttpError(400, "Email already exists");
         return next(error);
     }
 
     const hashPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+        name,
+        email,
+        password: hashPassword,
+    });
+
+    // Token
+    const token = jwt.sign(
+        {
+            sub: newUser._id,
+        },
+        config.jwtSecret,
+        {
+            expiresIn: "7h",
+        }
+    );
+
+    return res.status(201).json({
+        message: "User created successfully",
+        newUser: newUser,
+        token: token,
+    });
 };
 
 module.exports = {
